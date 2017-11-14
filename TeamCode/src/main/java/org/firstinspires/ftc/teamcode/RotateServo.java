66e596bc85dfdb11b37d922114d3e45f4cb00ac8
Neeka <bh266726@gmail.com>
2017-11-14 16:21:15 -0600

    Commit by Neeka on 11/14/2017
    Changed color sensor program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateServo.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -12,10 +13,12 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class RotateServo extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
     Servo servo;
+
     double servoPosition=1.0;
 
     @Override
     public void runOpMode() throws InterruptedException {
+
         telemetry.addData("Status","Initialized");
         telemetry.update();
         servo=hardwareMap.servo.get("servo");
