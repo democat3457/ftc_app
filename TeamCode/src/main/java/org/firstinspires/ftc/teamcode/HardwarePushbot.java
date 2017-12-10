91873d145c3d185f80d20156e134fc9bb8185585
FTCSchim - Colin <bh266726@gmail.com>
2017-12-10 17:04:24 -0600

    Solves error for 12/10/17
    Solves "Attempt to invoke method... " error with DcMotor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
@@ -93,7 +93,7 @@ public class HardwarePushbot {
         // Define and Initialize Motors
         leftDrive = hwMap.get(DcMotor.class, "right_drive");
         rightDrive = hwMap.get(DcMotor.class, "left_drive");
-        //leftArm = hwMap.get(DcMotor.class, "left_arm");
+        leftArm = hwMap.get(DcMotor.class, "left_arm");
         thirdWheel = hwMap.get(DcMotor.class, "third_wheel");
         leftDrive.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
@@ -102,14 +102,14 @@ public class HardwarePushbot {
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        //leftArm.setPower(0);
+        leftArm.setPower(0);
         thirdWheel.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         thirdWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define and initialize ALL installed servos.
