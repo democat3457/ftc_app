fa80b3bfa234131c749f7dc2f562dec06e8cbcb4
Colin Wong <cwongmath@gmail.com>
2020-01-23 20:06:40 -0600

    javadoc start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivebase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivebase.java
@@ -39,20 +39,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Constants;
 
 /**
- * This is NOT an opmode.
- * <p>
- * This class can be used to define all the specific hardware for a single robot.
- * In this case that robot is a Pushbot.
- * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
- * <p>
- * This hardware class assumes the following device names have been configured on the robot:
- * Note:  All names are lower case and some have single spaces between words.
- * <p>
- * Motor channel:  Left  drive motor:        "left_drive"
- * Motor channel:  Right drive motor:        "right_drive"
- * Motor channel:  Manipulator drive motor:  "left_arm"
- * Servo channel:  Servo to open left claw:  "left_hand"
- * Servo channel:  Servo to open right claw: "right_hand"
+ * Class that defines drivebase components and sensors.
  */
 
 public class Drivebase extends Constants {
@@ -139,6 +126,10 @@ public class Drivebase extends Constants {
 
     public HardwareMap getHardwareMap() { return hwMap; }
 
+    /** 
+     * Gets left drive 
+     * @return DcMotor
+     */
     public DcMotor getLeftDrive() { return leftDrive; }
     public DcMotor getRightDrive() { return rightDrive; }
     public DcMotor getThirdWheel() { return thirdWheel; }
